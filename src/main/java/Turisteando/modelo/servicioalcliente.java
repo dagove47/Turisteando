97375b1e3563c258f4ab2bed889b0dd56a1d005d
Franciny <franma3103@gmail.com>
2023-04-23 09:26:35 -0600

    fran test

--- src/main/java/Turisteando/modelo/servicioalcliente.java
+++ src/main/java/Turisteando/modelo/servicioalcliente.java
@@ -1,18 +1,20 @@
 
 package Turisteando.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Entity
 @Table(name= "servicioalcliente")
 
 public class servicioalcliente {
-     @Id
+
+    @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String fechadesolictud;
---
+++ src/main/java/Turisteando/service/UserService.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Turisteando.service;
+
+import Turisteando.interfaceService.IpersonaService;
+import Turisteando.modelo.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author fagueros
+ */
+@Service
+public class UserService implements UserDetailsService{
+    
+    @Autowired
+    public IpersonaService personaService;
+
+    @Override
+    public UserDetails loadUserByUsername(String Email) throws UsernameNotFoundException {
+       Persona persona = this.personaService.findbyNombre(Email);
+       Userprincipal userprincipal = new Userprincipal(persona);
+       return userprincipal;
+    }
+    
+    
+}
